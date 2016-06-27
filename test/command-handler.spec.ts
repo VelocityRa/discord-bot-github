7658066c43b7e310c1733b3ef750de111ed8ecbb
Dylan Falconer <me@falconerd.com>
2016-06-27 21:19:52 +1000

    Added tests

---
+++ test/command-handler.spec.ts
@@ -0,0 +1,9 @@
+
+
+describe("CommandHandler", function() {
+  describe("#validate", function() {
+    it("should return a boolean as to this command's validity", function() {
+      assert.equal(false, true);
+    });
+  });
+});
